25f2747b94ac64503a98cb08a33e9500adf1eee4
yeona <215266@jnu.ac.kr>
2024-04-18 00:06:40 +0900

    First commit to connect with GitHub

---
+++ app/src/main/java/com/example/myapplication/ui/board/BoardFragment.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.ui.board;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class BoardFragment extends Fragment {
+
+    private BoardViewModel mViewModel;
+
+    public static BoardFragment newInstance() {
+        return new BoardFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_board, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(BoardViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/board/BoardViewModel.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui.board;
+
+import androidx.lifecycle.ViewModel;
+
+public class BoardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+
+}
+
